157994042fe175a0c32ddf920df4fe057e063b49
Brad Traversy <traversymedia@gmail.com>
2021-05-19 16:04:27 -0400

    Category label component and blog page

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import CategoryLabel from './CategoryLabel'
 
 export default function Post({ post }) {
   return (
@@ -16,7 +17,7 @@ export default function Post({ post }) {
         <span className='font-light text-gray-600'>
           {post.frontmatter.date}
         </span>
-        <div>{post.frontmatter.category}</div>
+        <CategoryLabel>{post.frontmatter.category}</CategoryLabel>
       </div>
 
       <div className='mt-2'>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,47 @@
+import fs from 'fs'
+import path from 'path'
+import Link from 'next/link'
+import matter from 'gray-matter'
 import Layout from '../../components/Layout'
+import Post from '../../components/Post'
+import { sortByDate } from '../../utils'
 
-export default function BlogPage() {
+export default function BlogPage({ posts }) {
   return (
     <Layout>
-      <h1>Blog</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    )
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  }
 }
