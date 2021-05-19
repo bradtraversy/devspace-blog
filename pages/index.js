ab9444d8aa602188690f303751fbb4013f634445
Brad Traversy <traversymedia@gmail.com>
2021-05-19 15:51:42 -0400

    Sort posts by date

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import fs from 'fs'
 import path from 'path'
+import Link from 'next/link'
 import matter from 'gray-matter'
 import Layout from '../components/Layout'
+import Post from '../components/Post'
+import { sortByDate } from '../utils'
 
 export default function HomePage({ posts }) {
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+
+      <Link href='/blog'>
+        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
+          All Posts
+        </a>
+      </Link>
     </Layout>
   )
 }
@@ -32,7 +47,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   }
 }
