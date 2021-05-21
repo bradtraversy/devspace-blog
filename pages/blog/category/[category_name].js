eaab0b7f61e10cdbab2eeceaa19739f16a1994ad
Brad Traversy <traversymedia@gmail.com>
2021-05-21 12:09:37 -0400

    category sidebar

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -3,20 +3,29 @@ import path from 'path'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
 import Post from '@/components/Post'
+import CategoryList from '@/components/CategoryList'
 import matter from 'gray-matter'
 import { getPosts } from '@/lib/posts'
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>
-        Posts in {categoryName}
-      </h1>
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>
+            Posts in {categoryName}
+          </h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   )
@@ -51,6 +60,10 @@ export async function getStaticProps({ params: { category_name } }) {
 
   const posts = getPosts()
 
+  // Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category)
+  const uniqueCategories = [...new Set(categories)]
+
   // Filter posts by category
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category.toLowerCase() === category_name
@@ -60,6 +73,7 @@ export async function getStaticProps({ params: { category_name } }) {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   }
 }
--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -3,21 +3,30 @@ import path from 'path'
 import Layout from '@/components/Layout'
 import Post from '@/components/Post'
 import Pagination from '@/components/Pagination'
+import CategoryList from '@/components/CategoryList'
 import { POSTS_PER_PAGE } from '@/config/index'
 import { getPosts } from '@/lib/posts'
 
-export default function BlogPage({ posts, numPages, currentPage }) {
+export default function BlogPage({ posts, numPages, currentPage, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+
+          <Pagination currentPage={currentPage} numPages={numPages} />
+        </div>
+
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
-
-      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   )
 }
@@ -48,6 +57,10 @@ export async function getStaticProps({ params }) {
 
   const posts = getPosts()
 
+  // Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category)
+  const uniqueCategories = [...new Set(categories)]
+
   const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
   const pageIndex = page - 1
   const orderedPosts = posts.slice(
@@ -60,6 +73,7 @@ export async function getStaticProps({ params }) {
       posts: orderedPosts,
       numPages,
       currentPage: page,
+      categories: uniqueCategories,
     },
   }
 }
