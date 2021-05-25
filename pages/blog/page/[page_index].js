2a5dc43a57d00b4e121d2e7dd89c97f0b88d79b1
Brad Traversy <traversymedia@gmail.com>
2021-05-25 13:21:14 -0400

    Fixed column layout

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -10,11 +10,11 @@ import { getPosts } from '@/lib/posts'
 export default function BlogPage({ posts, numPages, currentPage, categories }) {
   return (
     <Layout>
-      <div className='flex justify-between'>
+      <div className='flex justify-between flex-col md:flex-row'>
         <div className='w-3/4 mr-10'>
           <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
 
-          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          <div className='grid grid-cols-2 gap-5'>
             {posts.map((post, index) => (
               <Post key={index} post={post} />
             ))}
