80de1c381d49c7eb6996e45c8002b7f6f3638d8a
Brad Traversy <traversymedia@gmail.com>
2021-05-25 16:25:32 -0400

    Fixed responsiveness

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -11,7 +11,7 @@ export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
       <div className='flex justify-between'>
-        <div className='w-3/4 mr-10'>
+        <div className='md:w-3/4 mr-10'>
           <h1 className='text-5xl border-b-4 p-5 font-bold'>
             Posts in {categoryName}
           </h1>
@@ -23,7 +23,7 @@ export default function CategoryBlogPage({ posts, categoryName, categories }) {
           </div>
         </div>
 
-        <div className='w-1/4'>
+        <div className='md:w-1/4'>
           <CategoryList categories={categories} />
         </div>
       </div>
--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -11,7 +11,7 @@ export default function BlogPage({ posts, numPages, currentPage, categories }) {
   return (
     <Layout>
       <div className='flex justify-between flex-col md:flex-row'>
-        <div className='w-3/4 mr-10'>
+        <div className='md:w-3/4 mr-10'>
           <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
 
           <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
@@ -23,7 +23,7 @@ export default function BlogPage({ posts, numPages, currentPage, categories }) {
           <Pagination currentPage={currentPage} numPages={numPages} />
         </div>
 
-        <div className='w-1/4'>
+        <div className='md:w-1/4'>
           <CategoryList categories={categories} />
         </div>
       </div>
