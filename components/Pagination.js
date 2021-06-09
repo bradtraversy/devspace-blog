76fb9e73225bd8ad87fd2431d072043bfc6a4fe5
Will Adams <bushblade@gmail.com>
2021-06-09 11:40:13 +0100

    Pagination needs a key prop
    This fixes the warning
    Each child in a list should have a unique "key" prop 
    In the Pagination component.

--- components/Pagination.js
+++ components/Pagination.js
@@ -19,7 +19,7 @@ export default function Pagination({ currentPage, numPages }) {
           </Link>
         )}
         {Array.from({ length: numPages }, (_, i) => (
-          <Link href={`/blog/page/${i + 1}`}>
+          <Link href={`/blog/page/${i + 1}`} key={`page-${i}`}>
             <li className='relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer'>
               {i + 1}
             </li>
